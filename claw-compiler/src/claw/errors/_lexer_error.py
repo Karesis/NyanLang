028a034ed06dbc4e1ec594389a205c533cd1901e
Karesis <yangyifeng23@mails.ucas.ac.cn>
2025-07-01 21:04:58 +0800

    feat(errors): rename error handling files

--- claw-compiler/src/claw/errors/_lexer_error.py
+++
@@ -1,8 +0,0 @@
-from ._base import ClawError
-
-class LexerError(ClawError):
-    """
-    Indicates an error during the lexing (tokenization) phase.
-    For example, an unrecognized character.
-    """
-    pass
